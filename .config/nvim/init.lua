acae63bea8ee36e5072bfe27cef875962cb3a657
jeffzh4ng <zhang.jeffreyd@gmail.com>
2023-03-05 08:29:20 -0500

    Configure emmet lsp in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -467,6 +467,21 @@ mason_lspconfig.setup_handlers {
   end,
 }
 
+local lspconfig = require('lspconfig')
+lspconfig.emmet_ls.setup({
+    -- on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+        },
+      },
+    }
+})
+
 
 
 
